db1718637881b4fcdd28d6da439f29bb0b4b5fae
DateSerraTeste <dataserrateste@gmail.com>
2024-11-07 10:27:11 -0300

    Criação de botão para carregar pokemons na página principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pokedex';
-  numbers: number[] = Array.from({ length: 30 }, (_, i) => i + 1);
+  numbers: number[] = Array.from({ length: 1025}, (_, i) => i + 1);
 
   ngOnInit(): void { 
     
   }
+
+  
+
+  displayedImages: number[] = [];
+  imagesPerPage: number = 12;
+  currentPage: number = 0;
+
+  constructor() {
+    this.loadMoreImages();
+  }
+
+  get hasMoreImages(): boolean {
+    return this.currentPage * this.imagesPerPage < this.numbers.length;
+  }
+
+  loadMoreImages(): void {
+    const nextPageImages = this.numbers.slice(
+      this.currentPage * this.imagesPerPage,
+      (this.currentPage + 1) * this.imagesPerPage
+    );
+    this.displayedImages = [...this.displayedImages, ...nextPageImages];
+    this.currentPage++;
+  }
+
 }

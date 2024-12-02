09addbaa49e4ffb67449022675106e56dac800dd
DateSerraTeste <dataserrateste@gmail.com>
2024-12-02 13:49:03 -0300

    Criação de funções de start e reset para catchgame

--- src/app/components/catchgame/catchgame.component.ts
+++ src/app/components/catchgame/catchgame.component.ts
@@ -12,28 +12,58 @@ export class CatchgameComponent {
     [{ id: 7 }, { id: 8 }, { id: 9 }],
   ];
 
-  currentTime = 60;
+ 
+  timer: number = 60; 
+  timerInterval: any; 
+  gameOver: boolean = false;
+  isPlaying: boolean = false;
   score = 0;
   hitPosition: number | null = null;
   gameInterval: any;
   countDownInterval: any;
 
+  
   ngOnInit() {
-    this.startGame();
+    this.resetGame();
   }
 
   startGame() {
-    this.gameInterval = setInterval(() => this.randomSquare(), 1000);
-    this.countDownInterval = setInterval(() => this.countDown(), 1000);
+    this.isPlaying = true;
+    this.startTimer();
+    // this.gameInterval = setInterval(() => this.randomSquare(), 1000);
+    // this.countDownInterval = setInterval(() => this.countDown(), 1000);
   }
 
-  countDown() {
-    this.currentTime--;
-    if (this.currentTime <= 0) {
-      clearInterval(this.gameInterval);
-      clearInterval(this.countDownInterval);
-      // alert(`Game Over! O seu resultado foi: ${this.score}`);
-    }
+  resetGame() {
+    this.timer = 60; 
+    this.gameOver = false;
+    this.hitPosition = null;
+    this.score = 0;
+    clearInterval(this.gameInterval);
+    clearInterval(this.timerInterval);
+    this.isPlaying = false;
+  }
+
+  // countDown() {
+  //   this.currentTime--;
+  //   if (this.currentTime <= 0) {
+  //     clearInterval(this.gameInterval);
+  //     clearInterval(this.countDownInterval);
+  //     // alert(`Game Over! O seu resultado foi: ${this.score}`);
+  //   }
+  // }
+
+  startTimer() {
+    this.gameInterval = setInterval(() => this.randomSquare(), 1000);
+    this.timerInterval = setInterval(() => {
+      this.timer--;
+
+      if (this.timer <= 0) {
+        clearInterval(this.gameInterval);
+        clearInterval(this.timerInterval); // Para o timer ao final
+        this.gameOver = true
+      }
+    }, 1000);
   }
 
   randomSquare() {
@@ -44,13 +74,13 @@ export class CatchgameComponent {
     if (squareId === this.hitPosition) {
       this.score++;
       this.hitPosition = null;
-      // this.playSound('hit');
+      this.playSound('hit');
     }
   }
 
-  // playSound(audioName: string) {
-  //   const audio = new Audio(`../../../assets/audios/${audioName}.m4a`);
-  //   audio.volume = 0.2;
-  //   audio.play();
-  // }
+  playSound(audioName: string) {
+    const audio = new Audio(`../../../assets/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+  }
 }

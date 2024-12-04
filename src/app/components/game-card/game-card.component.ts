8387b3283871488732bc22d0eadbe6b9ce50355e
dataserrateste <dataserrateste@gmail.com>
2024-12-04 15:16:03 -0300

    alteração página games

--- src/app/components/game-card/game-card.component.ts
+++ src/app/components/game-card/game-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-game-card',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class GameCardComponent {
 
+  @Input() titulo: string = ''
+  @Input() imgGame: string = ''
 }
